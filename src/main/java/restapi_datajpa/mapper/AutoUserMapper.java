1cb96305f8357f000d9272045358314a707a2df1
Sourabh Aggarwal <devsourabhagg@gmail.com>
2025-02-16 13:53:08 +0530

    MapStruct added

---
+++ src/main/java/restapi_datajpa/mapper/AutoUserMapper.java
@@ -0,0 +1,18 @@
+//package restapi_datajpa.mapper;
+//
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//import org.mapstruct.factory.Mappers;
+//import restapi_datajpa.dto.UserDto;
+//import restapi_datajpa.entity.User;
+//
+//@Mapper
+//public interface AutoUserMapper {
+//
+//    AutoUserMapper MAPPER = Mappers.getMapper(AutoUserMapper.class);
+//
+////    @Mapping(source = "email",target = "emailAddress")
+//    UserDto mapToUserDto(User user);
+//
+//    User mapToUser(UserDto userDto);
+//}
